dde26da8d7bdbb999a33bd966907857b82255baf
egmCeiRyu <egami@ceiryu.com>
2025-06-27 13:17:07 +0900

    commit

---
+++ firebase-config.sample.js
@@ -0,0 +1,10 @@
+// firebase-config.sample.js
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "your-app.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-app.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
+  };
