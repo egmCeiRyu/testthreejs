9e587ba59d04e5e1f3221665765e6559de04405a
egmCeiRyu <egami@ceiryu.com>
2025-06-27 10:23:49 +0900

    commit

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCcO5afAW-juhjVqrLekagwPiyCsYaGmOY",
+    authDomain: "modelssample-3aea1.firebaseapp.com",
+    projectId: "modelssample-3aea1",
+    storageBucket: "modelssample-3aea1.firebasestorage.app",
+    messagingSenderId: "146909696204",
+    appId: "1:146909696204:web:78a6416df4290e44ea3269",
+    measurementId: "G-FWLDBM99JX"
+  };
